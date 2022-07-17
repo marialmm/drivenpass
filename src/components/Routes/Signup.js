06616e940ec0a05a576dd00afaeade8327d47567
Maria Luiza <maria.lmm.975@gmail.com>
2022-07-17 18:05:51 -0300

    feat: add connection to api in component Signup

--- src/components/Routes/Signup.js
+++ src/components/Routes/Signup.js
@@ -1,27 +1,71 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { IoLockClosed } from "react-icons/io5";
 import { useState } from "react";
+
+import { api } from "../../utils/api";
 import SignupErrorPopUp from "../Layout/PopUp/SignupErrorPopUp";
 
 export default function Signup() {
     const [popUp, setPopUp] = useState(false);
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    });
+
+    const navigate = useNavigate();
+
+    function sendUserData(e){
+        e.preventDefault();
+
+        const promise = api.post("/signup", user);
+        promise.then(()=>{
+            navigate("/");
+        });
+        promise.catch(err => {
+            console.log(err.response.data);
+            if(err.response.status === 409){
+                setPopUp(true);
+            }
+
+        })
+    }
 
     return (
         <Main>
             <IoLockClosed />
             <h1>DrivenPass</h1>
-            <form>
+            <form onSubmit={sendUserData}>
                 <label htmlFor="email">Usuário (e-mail)</label>
-                <input type="text" name="email" required />
+                <input
+                    type="email"
+                    name="email"
+                    value={user.email}
+                    onChange={(e) =>
+                        setUser({ ...user, email: e.target.value })
+                    }
+                    required
+                />
                 <label htmlFor="password">Senha</label>
-                <input type="password" name="password" required />
+                <input
+                    type="password"
+                    name="password"
+                    value={user.password}
+                    onChange={(e) =>
+                        setUser({ ...user, password: e.target.value })
+                    }
+                    required
+                />
                 <button type="submit">Criar</button>
             </form>
             <Link to="/">
                 <button>{"<"} Voltar</button>
             </Link>
-            {popUp ? <SignupErrorPopUp popUp={popUp} setPopUp={setPopUp} /> : <></>}
+            {popUp ? (
+                <SignupErrorPopUp popUp={popUp} setPopUp={setPopUp} />
+            ) : (
+                <></>
+            )}
         </Main>
     );
 }
